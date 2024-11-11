55e3a5a19df3d7709e3bd9161e5e47c75c07fbb7
aahil-r <aahil.rahman@laetottenham.ac.uk>
2024-11-11 12:27:20 +0000

    beginning of the end...

---
+++ more-oop.py
@@ -0,0 +1,28 @@
+class employee:
+    def __init__(self, passed_name, passed_role, passed_salary, passed_dob):
+        self.name = passed_name
+        self.role = passed_role
+        self.salary = passed_salary
+        self.dob = passed_dob
+    
+
+    def increase_salary(self, amount):
+        self_salary += amount
+    
+    def calculate_age(self):
+        return
+    
+class manager:
+    def __init__(self, passed_name, passed_role, passed_salary, passed_dob, passed_bonus):
+        self.name = passed_name
+        self.role = passed_role
+        self.salary = passed_salary
+        self.dob = passed_dob
+        self.bonus = passed_bonus
+    
+    def increase_salary(self, amount):
+        self_salary += amount
+    
+    def 
+
+        
